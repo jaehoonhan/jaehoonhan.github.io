6cafc28dfed9b1533bcbdc87d8d3c05c0699f114
Jay Han <jaehoonhan1@gmail.com>
2021-05-29 03:56:57 -0400

    Add mobile first responive  Home layout

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,34 @@
-/*==================== MENU SHOW Y HIDDEN ====================*/
+/*==================== MENU SHOW HIDDEN ====================*/
+const navMenu = document.getElementById('nav-menu'), 
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close')
 
 
 /*===== MENU SHOW =====*/
 /* Validate if constant exists */
-
+if (navToggle) {
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
 
 /*===== MENU HIDDEN =====*/
 /* Validate if constant exists */
-
+if (navClose) {
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
 
 /*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.querySelectorAll('.nav_link')
 
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    // Remove the show-menu class when clicking a nav_link
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*==================== ACCORDION SKILLS ====================*/
 
